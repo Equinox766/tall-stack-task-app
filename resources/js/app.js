a395b59b4c1826a904a8b3b42cc7205c5b7a4cba
Equinox766 <Equinox766@gmail.com>
2023-07-20 10:47:36 -0400

    FIX Tailwind config

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,7 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs'
-import FormsAlpinePlugin from '../../vendor/filament/forms/dist/module.esm'
-import focus from '@alpinejs/focus';
-import NotificationsAlpinePlugin from '../../vendor/filament/notifications/dist/module.esm'
+import Alpine from 'alpinejs';
 
-Alpine.plugin(FormsAlpinePlugin)
-Alpine.plugin(focus);
-Alpine.plugin(NotificationsAlpinePlugin)
+window.Alpine = Alpine;
 
-window.Alpine = Alpine
-
-Alpine.start()
+Alpine.start();
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,33 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
     ],
-
+    darkMode: "class",
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                danger: colors.rose,
+                primary: colors.teal,
+                success: colors.green,
+                warning: colors.yellow,
+                secondary: colors.rose,
+            },
         },
     },
-
-    plugins: [forms],
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+        forms,
+    ],
 };
